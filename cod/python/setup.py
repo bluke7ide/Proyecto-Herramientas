e7659ac8b8d67f7f0c89651f899b63f0d3944c98
Javier Hernández <javierhdezn@gmail.com>
2024-07-06 18:30:37 -0600

    docs: se agrega el setup de Python

---
+++ cod/python/setup.py
@@ -0,0 +1,9 @@
+# Módulos
+import pandas as pd
+import time
+import nltk
+from datetime import datetime, timedelta
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from deep_translator import GoogleTranslator
+from textblob import TextBlob
+
